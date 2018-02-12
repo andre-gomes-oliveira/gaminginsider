3239180713b7b30e8ee119214cd4d00cc2b41f4e
André Gomes de Oliveira <andre.gomes.olivera@gmail.com>
2018-02-12 12:29:22 -0200

    Initial commit - Step 01 of the implementation plan completed
    A skeleton application has been created;
    The planned dependencies gave been added;
    A signing configuration and key have been created;

---
+++ app/src/main/java/br/com/andre/gomes/oliveira/gaminginsider/MainActivity.java
@@ -0,0 +1,57 @@
+package br.com.andre.gomes.oliveira.gaminginsider;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import timber.log.Timber;
+
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     The activity's Toolbar
+     */
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Setting up Timber
+        Timber.plant(new Timber.DebugTree());
+
+        //Setting up ButterKnife
+        ButterKnife.bind(this);
+
+        setSupportActionBar(mToolbar);
+        mToolbar.setTitle(getTitle());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/br/com/andre/gomes/oliveira/gaminginsider/MainActivityFragment.java
@@ -0,0 +1,23 @@
+package br.com.andre.gomes.oliveira.gaminginsider;
+
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+}
