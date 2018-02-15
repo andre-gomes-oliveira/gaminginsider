647c5c5c11fa9d637e5ac1eea13117c5e70e07eb
André Gomes de Oliveira <andre.gomes.olivera@gmail.com>
2018-02-14 23:03:36 -0200

    feat: Initial test and implementation of firebase and a RSS parser library.

--- app/src/main/java/br/com/andre/gomes/oliveira/gaminginsider/MainActivityFragment.java
+++ app/src/main/java/br/com/andre/gomes/oliveira/gaminginsider/MainActivityFragment.java
@@ -7,12 +7,39 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+//testing
+import com.prof.rssparser.Article;
+import com.prof.rssparser.Parser;
+
+import java.util.ArrayList;
+
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
 
     public MainActivityFragment() {
+
+        String urlString = "http://feeds.ign.com/ign/all";
+        Parser parser = new Parser();
+        parser.execute(urlString);
+        parser.onFinish(new Parser.OnTaskCompleted() {
+
+            @Override
+            public void onTaskCompleted(ArrayList<Article> articles){
+
+                String title = articles.get(0).getTitle();
+                String description = articles.get(0).getDescription();
+                String content = articles.get(0).getContent();
+                //what to do when the parsing is done
+                //the Array List contains all article's data. For example you can use it for your adapter.
+            }
+
+            @Override
+            public void onError() {
+                //what to do in case of error
+            }
+        });
     }
 
     @Override
